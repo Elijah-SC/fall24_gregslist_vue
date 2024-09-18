e74c2b082b1dda4350b642b286fc823e4f2e44cb
Elijah SasserCollins <ElijahJSasserCollins@gmail.com>
2024-09-18 14:19:50 -0600

    dumping raw house data in page

--- src/pages/HousesPage.vue
+++ src/pages/HousesPage.vue
@@ -1,9 +1,11 @@
 <script setup>
+import { AppState } from "@/AppState.js";
 import { housesService } from "@/services/HousesService.js";
 import { logger } from "@/utils/Logger.js";
 import Pop from "@/utils/Pop.js";
-import { onMounted } from "vue";
+import { computed, onMounted } from "vue";
 
+const houses = computed(() => AppState.houses)
 
 onMounted(() =>
   getHouses()
@@ -22,8 +24,14 @@ async function getHouses() {
 
 <template>
 
-  <body>
-    <h1>Welcome to the Houses Page</h1>
+  <body class="container">
+    <section class="row">
+      <h1>Welcome to the Houses Page</h1>
+      <div v-for="house in houses" :key="house.id" class="col-12">
+        {{ house }}
+      </div>
+    </section>
+
   </body>
 </template>
 
