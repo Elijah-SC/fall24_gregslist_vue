88ff17959ec071184046a0a3d72e57a7644a69a7
Elijah SasserCollins <ElijahJSasserCollins@gmail.com>
2024-09-18 14:13:28 -0600

    made Houses Model

---
+++ src/pages/HousesPage.vue
@@ -0,0 +1,31 @@
+<script setup>
+import { housesService } from "@/services/HousesService.js";
+import { logger } from "@/utils/Logger.js";
+import Pop from "@/utils/Pop.js";
+import { onMounted } from "vue";
+
+
+onMounted(() =>
+  getHouses()
+)
+
+async function getHouses() {
+  try {
+    await housesService.getHouses()
+  } catch (error) {
+    Pop.meow(error)
+    logger.log(error)
+  }
+}
+</script>
+
+
+<template>
+
+  <body>
+    <h1>Welcome to the Houses Page</h1>
+  </body>
+</template>
+
+
+<style lang="scss" scoped></style>
